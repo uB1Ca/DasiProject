7cd83afbff93548ba86de8fe968daf9017978123
uBiCa <ushomosiashvili@gmail.com>
2023-09-15 16:27:43 +0400

    Character Controller + Camera

---
+++ Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorController : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void PlayIdle()
+    {
+        _animator.Play("Idle");
+    }
+    
+    public void PlayWalk()
+    {
+        _animator.Play("Walk");
+    }
+}
